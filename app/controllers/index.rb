5d07f663aa798d0cafdd2b9c9c610c4cded9a36d
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-04 19:46:08 -0500

    add users categories user input form

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,22 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/users' do
+  @users = User.all 
+  erb :user_index
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :user_id
+end
+
+post '/users' do
+  # @user = User.create(params)
+  # @user = User.create(:first_name => params[:first_name], :last_name => params[:last_name])
+  @user = User.create(params.slice(:first_name, :last_name))
+
+  redirect to('/users')
+end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
   has_many :posts
+
+  def name
+    first_name + " " + last_name
+  end
+
 end
